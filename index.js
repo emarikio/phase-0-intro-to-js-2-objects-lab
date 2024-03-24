4ab2999090d3920d79e7b9f3b927dfb9650af0fa
Emarilyne Wakio <emarilynew@gmail.com>
2024-03-24 14:16:04 +0300

    The objects lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, Kelvin) {
+    const newEmployee = {...employee};
+    newEmployee[name] = Kelvin;
+    return newEmployee
+    };
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Kelvin){
+    employee[name] = Kelvin;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const newEmployee = {...employee};
+    delete newEmployee[name];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+    return employee;
+}
+    
+
